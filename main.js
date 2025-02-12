68453bd93b719f3353090aef9acc2f683ce8fc4d
AuraRumeur Zbs <63426480+AuraRumeur0Zbs@users.noreply.github.com>
2025-02-12 14:09:47 +0100

    init comit

---
+++ main.js
@@ -0,0 +1,23 @@
+// JavaScript Document
+
+//function generatevideo () {
+//	var nombre = Math.floor(Math.random() * 3);
+//	const element = document.getElementById("videodiv")
+//	const text = document.createTextNode("TEST")
+//	const child = document.getElementById("para")
+//	const p = document.createElement("p")
+////	const newVideo = document.createElement('video')
+////	
+////	newVideo.src = "1.mp4"
+////	newVideo.id = "video-carrousel"
+////	newVideo.muted = true
+////	element.insertBefore(newVideo);
+////		= `<video id="video-carrousel" autoplay muted src="`+ nombre + `.mp4" type="video/mp4"> video not supported </video>`
+//	p.appendChild(text)
+//	element.insertBefore(text, p);
+//	document.getElementById('video-carrousel').addEventListener('ended',myHandler,false);
+//}
+//generatevideo()
+//function myHandler() {
+//	generatevideo()
+//}
